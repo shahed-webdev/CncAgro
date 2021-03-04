90e7acbdced87324a54396beae34f7106627b5d1
Shahed Al Mahmud <shahedalmahmud@outlook.com>
2021-03-04 17:19:04 +0600

    Admin product distribution

---
+++ AccessAdmin/Member/ProdcutDistribution/Delivery_Details.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Web.UI.WebControls;
+
+namespace CncAgro.AccessAdmin.Member.ProductDistribution
+{
+    public partial class Delivery_Details : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(Request.QueryString["DistributionID"]))
+            {
+                Response.Redirect("Product_Delivery.aspx");
+            }
+        }
+
+        protected void Delivery_Button_Click(object sender, EventArgs e)
+        {
+
+            foreach (GridViewRow row in ProductGridView.Rows)
+            {
+                if (row.RowType == DataControlRowType.DataRow)
+                {
+
+                    Stock_UpdateSQL.UpdateParameters["Product_PointID"].DefaultValue = ProductGridView.DataKeys[row.RowIndex]["ProductID"].ToString();
+                    Stock_UpdateSQL.UpdateParameters["Stock_Quantity"].DefaultValue = ProductGridView.DataKeys[row.RowIndex]["SellingQuantity"].ToString();
+                    Stock_UpdateSQL.Update();
+
+                    Seller_Product_insert_UpdateSQL.InsertParameters["SellerID"].DefaultValue = InfoFormView.DataKey["SellerID"].ToString();
+                    Seller_Product_insert_UpdateSQL.InsertParameters["SellerProduct_Stock"].DefaultValue = ProductGridView.DataKeys[row.RowIndex]["SellingQuantity"].ToString();
+                    Seller_Product_insert_UpdateSQL.InsertParameters["Product_PointID"].DefaultValue = ProductGridView.DataKeys[row.RowIndex]["ProductID"].ToString();
+                    Seller_Product_insert_UpdateSQL.Insert();
+
+
+                }
+            }
+            ProductSQL.Update();
+
+            Response.Redirect("Product_Delivery.aspx");
+        }
+    }
+}
---
+++ AccessAdmin/Member/ProdcutDistribution/Order_Confirmation.aspx.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CncAgro.AccessAdmin.Member.ProductDistribution
+{
+    public partial class Order_Confirmation : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
